e970962e23eee783ecaf09203836ebdd97634a5f
ahpharjr <6531503132@lamduan.mfu.ac.th>
2024-11-07 21:29:18 +0700

    Add Owner home page

--- src/main/java/com/ONE4ALL/MFU_Canteen/Controller/OwnerHomeController.java
+++ src/main/java/com/ONE4ALL/MFU_Canteen/Controller/OwnerHomeController.java
@@ -1,6 +1,5 @@
 package com.ONE4ALL.MFU_Canteen.Controller;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ONE4ALL.MFU_Canteen.Entity.Owner;
-import com.ONE4ALL.MFU_Canteen.Entity.User;
-import com.ONE4ALL.MFU_Canteen.Repository.UserRepository;
 import com.ONE4ALL.MFU_Canteen.Service.OwnerService;
 
 @Controller
@@ -28,7 +25,7 @@ public class OwnerHomeController {
             model.addAttribute("owner", owner);
         }
 
-        return "owner-menu";  // This will display the owner's dashboard page
+        return "owner-menu"; 
     }
 }
 
